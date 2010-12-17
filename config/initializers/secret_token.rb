5c4476365a5eb10272138ae540eb70d80492a313
karim12345 <abdelkarim12345@hotmail.com>
2010-12-17 10:01:16 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectKarim::Application.config.secret_token = 'be536b51062b93483561d937ae957170e52db03748e62da336530badbad1ccf030f4c00430ccdbf9071bdfbfe22414161df91a75b4fafebef5ff77c8a35461ac'
